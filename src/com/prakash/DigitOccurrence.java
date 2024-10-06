e28fef27fe803dfcee71d12abe2ec04b3b05afc6
NavinPanthi <panthinabin341@gmail.com>
2024-10-06 17:51:14 +0545

    refactor : digit occurrence of number program

--- src/com/prakash/DigitOccurrence.java
+++ src/com/prakash/DigitOccurrence.java
@@ -1,4 +1,15 @@
 package com.prakash;
 
+import java.util.Scanner;
+
 public class DigitOccurrence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long number = sc.nextLong();
+        System.out.println("Enter the number from where you want digit occurrence.");
+        int n = sc.nextInt();
+        System.out.println("Enter the number of which you want digit occurrence");
+
+
+    }
 }
