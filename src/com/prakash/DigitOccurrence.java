7550e44934620423c01bbbc48239b732ddc2d6e5
NavinPanthi <panthinabin341@gmail.com>
2024-10-07 15:17:56 +0545

    refactor : digit occurrence of number program

--- src/com/prakash/DigitOccurrence.java
+++ src/com/prakash/DigitOccurrence.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class DigitOccurrence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long number = sc.nextLong();
         System.out.println("Enter the number from where you want digit occurrence.");
-        int n = sc.nextInt();
+        long number = sc.nextLong();
         System.out.println("Enter the number of which you want digit occurrence");
-
-
+        int n = sc.nextInt();
+        int count = 0;
+        while(number > 0){
+            long digit = number % 10;
+            if(digit == n){
+                count++;
+            }
+            number = number / 10;
+        }
+        System.out.println("The digit occurrence is\t" + count);
     }
 }
