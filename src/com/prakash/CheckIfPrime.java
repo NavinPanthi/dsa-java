80fb27e152c90c8926cb33886e2928fb9c8ec0a4
Navin Panthi <panthinabin341@gmail.com>
2024-10-10 00:51:02 +0545

    add : Prime number check program

---
+++ src/com/prakash/CheckIfPrime.java
@@ -0,0 +1,23 @@
+package com.prakash;
+
+import java.util.Scanner;
+
+public class CheckIfPrime {
+    public static void main(String[] args) {
+        System.out.println("Enter the number to check.");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i, count=0;
+        for(i=1; i<=n; i++){
+           if(n%i == 0){
+               count++;
+           }
+        }
+        if(count == 2){
+            System.out.println("The number\t" + n + "\tis prime.");
+        }
+        else{
+            System.out.println("The number\t" + n + "\tis not prime.");
+        }
+    }
+}
