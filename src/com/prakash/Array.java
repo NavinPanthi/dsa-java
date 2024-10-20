84d52abf3fbe5c61aafe0de583d239d0b38e8d4e
Navin Panthi <panthinabin341@gmail.com>
2024-10-20 09:49:29 +0545

    refactor: strings array input

--- src/com/prakash/Array.java
+++ src/com/prakash/Array.java
@@ -1,12 +1,14 @@
 package com.prakash;
 
+import java.util.Scanner;
+
 public class Array {
     public static void main(String[] args) {
         int [] arr = new int[5];
         //Primitive variables like x, y, and value are stored in the stack.
         //here new keyword means an arr object is created in heap memory.
         System.out.println(arr[0] + "" + arr[4]); // 00
-        String [] arr1 = new String[5]; //here new keyword means an arr object is created in heap memory.
+        String [] arr1 = new String[5];
         System.out.println(arr1[0] + "" + arr1[4]); //nullnull
 
         //null can not be assigned to primitive data type in java.
@@ -16,5 +18,16 @@ public class Array {
         // null can be assigned to secondary data type.
         String s = null;
 
+        //Input string as array
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i<5; i++){
+            arr1[i] = sc.next();
+        }
+        for(int i = 0; i<arr1.length; i++){
+            System.out.print(arr1[i] + "\t");
+        }
+        for (String string : arr1) {
+            System.out.print(string + "\t");
+        }
     }
 }
